742589a248fec1742eb17da5a76b3d7025603b09
haiqaldani <muhammadhaiqaldani@gmail.com>
2022-06-26 21:44:01 +0700

    fix controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Child;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -23,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $child = DB::table('child')->get();
+        $child = Child::all();
         return view('home',['child' => $child]);
     }
 }
--- app/Http/Controllers/ParentController.php
+++ app/Http/Controllers/ParentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Parents;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,18 +13,21 @@ class ParentController extends Controller
     }
 
     public function add(Request $request){
-        DB::table('parent')->insert([
-            'name'=>$request->namaAyah,
-            'age'=>$request->umurAyah,
-            'phone'=>$request->telpAyah,
-            'email'=>$request->emailAyah
-        ]);
-        DB::table('parent')->insert([
-            'name'=>$request->namaIbu,
-            'age'=>$request->umurIbu,
-            'phone'=>$request->telpIbu,
-            'email'=>$request->emailIbu
-        ]);
+
+        $data = $request->all();
+        Parents::create($data);
+        // DB::table('parent')->insert([
+        //     'name'=>$request->namaAyah,
+        //     'age'=>$request->umurAyah,
+        //     'phone'=>$request->telpAyah,
+        //     'email'=>$request->emailAyah
+        // ]);
+        // DB::table('parent')->insert([
+        //     'name'=>$request->namaIbu,
+        //     'age'=>$request->umurIbu,
+        //     'phone'=>$request->telpIbu,
+        //     'email'=>$request->emailIbu
+        // ]);
         return redirect('/');
     }
 }
